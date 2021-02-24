2cfa7bf15224978046d59ea787280640a6a88ccd
Maxwell Schutze <mbs45316@creighton.edu>
2021-02-24 16:15:44 -0600

    Steps 2a-h

---
+++ LAWeek4MaxwellSchutze.appstudio.appstudio/forms/pets/pets.js
@@ -0,0 +1,14 @@
+let petNames = ['Darcy', 'Jack', 'Jerry', 'Marty', 'Vito', 'Vinny', 'Riley', 'Saddie', 'Marge', 'Sam']
+
+let withSal = petNames
+withSal.push('Sal')
+
+thirdName = petNames[2]
+
+console.log(`The third pet is named ${thirdName}.`)
+
+petLength = petNames.length
+
+console.log(`The array has ${petLength} names in it.`)
+
+petNames.pop()
